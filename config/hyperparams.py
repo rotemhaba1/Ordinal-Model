2fb8dca70aa360ab55f0239476afb516cab1ff94
rotemhaba1 <rotemhaba2910@gmail.com>
2025-03-05 12:28:22 +0200

    Add WIGR_max to the results

--- config/hyperparams.py
+++ config/hyperparams.py
@@ -1,18 +1,27 @@
 param_grids = {
-
-
     "RandomForest": {},
-    "RandomForest_Ordinal":{'WIGR_power':[0.2,1,2],'criterion':['WIGR_EV','WIGR_mode','WIGR_min','WIGR_EV_fix']},
-    "DecisionTrees_Ordinal": {'WIGR_power':[0.2,1,2],'criterion':['WIGR_EV','WIGR_mode','WIGR_min','WIGR_EV_fix']},
+    "RandomForest_Ordinal":{'WIGR_power':[0.2,1,2],'criterion':['WIGR_EV','WIGR_mode','WIGR_min','WIGR_EV_fix','WIGR_max']},
+    "DecisionTrees_Ordinal": {'WIGR_power':[0.2,1,2],'criterion':['WIGR_EV','WIGR_mode','WIGR_min','WIGR_EV_fix','WIGR_max']},
 "XGBoost": {},
     "DecisionTrees": {
         "class_weight": [ 'balanced',None], },
     "AdaBoost": {},
 "catboost": {},
 
-    "AdaBoost_Ordinal": {'WIGR_power': [0.2, 1, 2], 'criterion': ['WIGR_EV', 'WIGR_mode', 'WIGR_min', 'WIGR_EV_fix'],
+    "AdaBoost_Ordinal": {'WIGR_power': [0.2, 1, 2],
+                         'criterion': ['WIGR_EV', 'WIGR_mode', 'WIGR_min', 'WIGR_EV_fix','WIGR_max'],
                          'algorithm': ['', 'SAMME_R', 'half', 'SAMME']},
 
 }
+
+param_grids = {
+    "RandomForest_Ordinal":{'WIGR_power':[0.2,1,2],'criterion':['WIGR_max']},
+    "DecisionTrees_Ordinal": {'WIGR_power':[0.2,1,2],'criterion':['WIGR_max']},
+    "AdaBoost_Ordinal": {'WIGR_power': [0.2, 1, 2],
+                         'criterion': ['WIGR_max', 'WIGR_EV_fix'],
+                         'algorithm': ['', 'SAMME_R', 'half', 'SAMME']},
+
+}
+
 
 # WIGR_max
