c833aacf429d7bd90938c04cb1605f126e5c2fb6
rotemhaba1 <rotemhaba2910@gmail.com>
2025-03-09 10:05:45 +0200

    finish independed

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,16 +1,4 @@
-import concurrent.futures
 
-
-def your_function(param):
-    print(f"Running with {param}")
-
-
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    future_X = executor.submit(your_function, 'X')
-    future_Y = executor.submit(your_function, 'Y')
-
-    concurrent.futures.wait([future_X, future_Y])
-
-    print(f"X finished: {future_X.done()}")
-    print(f"Y finished: {future_Y.done()}")
+#src.preprocessing.save_processed_data
+#src.training.save_processed_data
+#src.validation.save_processed_data
